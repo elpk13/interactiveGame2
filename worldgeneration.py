668dfbcdeb879e28945056a9377fabd306521d4b
RMRattray <rmr3141@gmail.com>
2026-03-16 09:41:00 -0400

    Spelling issue prevents file from loading

--- worldgeneration.py
+++ worldgeneration.py
@@ -70,7 +70,7 @@ def getWorldGraphics(window_height,worldx=0,worldy=0,bgname="Map_Background.png"
                 pframeus.append(frame)
                 frame = pygame.transform.scale(frame,(int(height*frame.get_width()/(12*frame.get_height())),int(height/12)))
                 pupframeus.append(frame)
-                frame = pygame.image.load(os.path.join('Animations','Wolves',wolfname,wolfname + '_walking_Away000' + str(f) + '.png'))
+                frame = pygame.image.load(os.path.join('Animations','Wolves',wolfname,wolfname + '_Walking_Away000' + str(f) + '.png'))
                 frame = pygame.transform.scale(frame,(int(height*frame.get_width()/(7*frame.get_height())),int(height/7)))
                 pframeds.append(frame)
                 frame = pygame.transform.scale(frame,(int(height*frame.get_width()/(9*frame.get_height())),int(height/9)))
